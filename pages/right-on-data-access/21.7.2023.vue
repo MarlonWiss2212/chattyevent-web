6474f2376531c3af292cecb05db620b7f02c3e8f
MarlonWiss2212 <marlon.wisskirchen@outlook.de>
2023-07-21 10:09:55 +0200

    fix

--- pages/right-on-data-access/21.7.2023.vue
+++ pages/right-on-data-access/21.7.2023.vue
@@ -1,2 +1,9 @@
 <template>
+  <div class="w-full p-5 overflow-y-auto">
+    <h1 class="text-3xl font-bold">Recht auf Auskunft ChattyEvent</h1>
+    <div class="mt-6">
+      <h2 class="font-bold text-xl">1. Allgemeines</h2>
+      <p class="mt-2">Auf dieser Seite werden ihnen alle Daten dargestellt die wir sammeln. Die Verarbeitung der Daten ist in der Datenschutzerklärung beschrieben</p>
+    </div>
+  </div>
 </template>
