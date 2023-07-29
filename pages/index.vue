83798f11e73332a144d6a6c416ea3bed7183b742
MarlonWiss2212 <marlon.wisskirchen@outlook.de>
2023-07-29 18:45:38 +0200

    fixed index site

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,21 @@
   <head>
     <title>ChattyEvent</title>
   </head>
-  <div>
+  <div class="">
+    <div class="flex flex-row">
+      <div class="w-0 flex-grow pr-10">
+        <h2 class="text-2xl md:text-5xl font-semibold">Plane ganz einfach deine Events, Partys und schreibe mit deinen Freunden</h2>
+        <h3>
+          <br>ChattyEvent bietet Ihnen die Möglichkeit, Gruppenchats zu erstellen und mit Ihren Freunden, Familie oder Kollegen zu kommunizieren. Egal ob Sie ein Event planen oder einfach nur in Kontakt bleiben wollen, un
+          <br><br>Mit der Event-Planungsfunktion können Sie schnell und einfach ein Event erstellen und die Details teilen. Sie können die Veranstaltung mit einem Gruppenchat verknüpfen oder auch ohne Chat planen. Mit dieser Funktion können Sie sicherstellen, dass jeder eingeladen ist und alle notwendigen Details auf einen Blick zur Verfügung stehen.
+          <br><br>ChattyEvent verfügt auch über ein Freundschaftssystem, das es Ihnen ermöglicht, die Berechtigungen Ihrer Freunde zu verwalten. Sie können bestimmte Freunde zu einer Gruppe hinzufügen und deren Zugriff auf die Gruppe steuern. Sie können auch individuelle Berechtigungen für jeden Freund festlegen, um sicherzustellen, dass Ihre Gruppe organisiert und sicher bleibt.
+          <br><br>Um Ihre Event-Planung noch einfacher zu gestalten, haben wir eine Einkaufsliste hinzugefügt. Mit dieser Funktion können Sie angeben, was jeder Event-Teilnehmer mitbringen soll, so dass Sie nichts vergessen und alles im Voraus organisiert haben.
+          <br><br>ChattyEvent ist einfach zu bedienen und bietet Ihnen alle Funktionen, die Sie benötigen, um Ihre Gruppenkommunikation und Event-Planung zu vereinfachen. Probieren Sie es jetzt aus und lassen Sie sich von der Leichtigkeit und Effizienz unserer App überzeugen.
+        </h3>
+      </div>
+      <div class="w-60">
+        <img class="w-full border-slate-500 border-4 rounded-xl" src="/chats_screenshot.jpg" />
+      </div>
+    </div>
   </div>
 </template>
