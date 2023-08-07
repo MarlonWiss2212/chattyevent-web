8e09b550a3d9531a690dbf8772977e44dd48dcec
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-08-07 15:28:09 +0200

    faq change

--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -10,6 +10,7 @@
     <h2 class="text-3xl font-semibold">Häufig gestellte Fragen</h2>
     <div class="flex flex-col mt-10 gap-3">
       <faq-container question="Werden Events automatisch gelöscht?" answer="Nein Events werden nicht automatisch gelöscht. Sie müssen manuell gelöscht werden" />
+      <faq-container question="Kann man Nachrichten im nachhinein Löschen?" answer="Nein, sobald Nachrichten versendet wurden können sie grundsätzlich nicht mehr gelöscht werden. Eine Ausnahme ist, wenn sie ihren gesamten Account löschen, dabei werden alle je geschriebenen Nachrichten gelöscht. Wir arbeiten an einem Feature, sodass man Einzelchats zu anderen Usern löschen kann, dieses wird allerdings frühestens im Q4 veröffentlicht werden" />
     </div>
   </div>
 </template>
