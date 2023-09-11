d3c123d2a3bd49a1e672c78f9c4e57a1868ae16c
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-09-11 15:44:33 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   app: {
     head: {
       link: [{ rel: 'icon', type: 'image/png', href: '/icon.png' }]
